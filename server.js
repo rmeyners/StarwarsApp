10bbcf3c5b2cdacba919972e3294dedefbf8dec8
rmeyners <robert.meyners@gmail.com>
2017-10-24 20:44:02 -0700

    changes and port

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var path = require("path");
 // =============================================================
 var app = express();
 // var PORT = 3000;
-
 var port = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
